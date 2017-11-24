f2be3e8e07e0e763277cb3e135a545ed3b0d95a7
Full <fullbl@mail.com>
2017-11-24 18:31:40 +0100

    send post data!

--- public/src/components/Form.vue
+++ public/src/components/Form.vue
@@ -1,17 +1,31 @@
 <template>
-  <form>
+  <form v-on:submit.prevent="save">
     <h1>{{ title }}</h1>
     <input-part v-for="input in inputs" key="input.name" v-bind:input="input"></input-part>
-    <input type="button" value="salva">
+    <input type="submit" value="salva">
   </form>
 </template>
 
 <script>
 import InputPart from './InputPart';
+import axios from 'axios';
+
 export default {
   name: 'Form',
   components: {
   	InputPart
+  },
+  methods: {
+    save: (event)=> {
+      let formData = new FormData(event.target);
+      axios.post('/message')
+      .then((response) => {
+        alert('ok');
+      })
+      .catch((response) => {
+        alert('ko');
+      });
+    }
   },
   props: ['inputs', 'title']  
 };
